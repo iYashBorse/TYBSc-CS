1c4e9229060d5128849e15da1b17da7f1f617951
BruceWayne <iyashborse@gmail.com>
2020-08-24 15:08:51 +0530

    Updated GetBirthDate.java
    Added comments to the program.

--- JAVA/Assignment-3/GetBirthDate.java
+++ JAVA/Assignment-3/GetBirthDate.java
@@ -7,16 +7,16 @@ public class GetBirthDate {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Please Enter Your Birth Date : ");
-        String birthDateString = input.nextLine();
+        String birthDateString = input.nextLine();			//Accept birthdate from user
 
-        String[] birthDate = birthDateString.split("/",3); 
-        int birthDay = Integer.parseInt(birthDate[0]);
-        int birthMonth = Integer.parseInt(birthDate[1]);
-        int birthYear = Integer.parseInt(birthDate[2]);
+        String[] birthDate = birthDateString.split("/",3); 		//return array where the elements are birthdate
+        int birthDay = Integer.parseInt(birthDate[0]);			//typecasting str to int
+        int birthMonth = Integer.parseInt(birthDate[1]);		//typecasting str to int
+        int birthYear = Integer.parseInt(birthDate[2]);			//typecasting str to int
         
-        Calendar today = Calendar.getInstance();
-        int todayDay = today.get(Calendar.DAY_OF_MONTH);
-        int todayMonth = today.get(Calendar.MONTH) + 1;
+        Calendar today = Calendar.getInstance();			//Calendar object created
+        int todayDay = today.get(Calendar.DAY_OF_MONTH);		//fetching today's date	
+        int todayMonth = today.get(Calendar.MONTH) + 1;			
         int todayYear = today.get(Calendar.YEAR);
 
         int years;
@@ -25,7 +25,17 @@ public class GetBirthDate {
 
         years = todayYear - birthYear;
 
-        if(todayMonth >= birthMonth){
+/* CodeLens - In this nested if block we check every condition for calculating age perfectly.
+One condition is permanent- Your birthyear is always less than today's year. 
+Second condition has two possibilities- first, if today's month greater than or equal to birth month, no
+worries simply subtract it but in second case if birth month is greater than today's month which means you haven't
+grown that old yet, so we decrement a year because you weren't one year old the instance you were born. 
+Also, you need to check if today's date is greater than or equal to your birth date, in the first case, if it is equal
+to or greater than, then simply subtract it otherwise if its less than, then we increment month by 1 because you're 
+already that many months old.
+*/        
+
+	if(todayMonth >= birthMonth){
             months = todayMonth - birthMonth;
             if(todayDay >= birthDay){
                 days = todayDay - birthDay;
